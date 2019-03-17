6070d222567975c96b9384aeb89e7eb632c989d1
郭甫松 <guofusong@outlook.com>
2019-03-17 11:51:41 +0800

    添加基础控制器。

---
+++ admin/src/main/java/fun/hijklmn/admin/controller/BaseController.java
@@ -0,0 +1,40 @@
+package fun.hijklmn.admin.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
+import fun.hijklmn.admin.conf.ConstantsConf;
+import fun.hijklmn.model.service.IDocumentService;
+import fun.hijklmn.model.service.IImageService;
+import fun.hijklmn.model.service.IMenuService;
+import fun.hijklmn.model.service.ISoundService;
+import fun.hijklmn.model.service.ISysUserService;
+
+public abstract class BaseController {
+
+	protected static final Logger logger = LoggerFactory.getLogger(BaseController.class);
+	
+	@Autowired
+	protected RestTemplate restTemplate;
+
+	@Autowired
+	protected ConstantsConf constantsConf; 
+	
+	@Autowired
+	protected ISysUserService sysUserService;
+
+	@Autowired
+	protected IMenuService menuService;
+	
+	@Autowired
+	protected ISoundService soundService;
+	
+	@Autowired
+	protected IImageService imageService;
+	
+	@Autowired
+	protected IDocumentService documentService;
+	
+}
